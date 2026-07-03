798773152e1b787fa1d18830c712825773d1fd42
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 00:07:39 +0200

    Update install_apt-abroad.sh

--- install_apt-abroad.sh
+++ install_apt-abroad.sh
@@ -4,7 +4,9 @@ echo "alias apt_CA='/etc/apt/install_apt-CA.sh'" >> ~/.bashrc
 sudo apt modernize-sources --yes
 mv /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian_MODEL.sources.disabled
 
-cat>/etc/apt/install_apt-abroad.sh<<'CAendof'
+
+
+cat>/etc/apt/install_apt-CA.sh<<'CAendof'
 sed 's/URIs: http:\/\/deb.debian.org\/debian\//URIs: https:\/\/mirror.cpsc.ucalgary.ca\/debian\//g' /etc/apt/sources.list.d/debian_MODEL.sources.disabled >  /etc/apt/sources.list.d/debian-abroad.sources
 mullvad relay set location ca
 
