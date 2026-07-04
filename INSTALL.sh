d9940704e8dd599108e99fff35e81b3d03175a32
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 18:43:34 +0200

    Update INSTALL.sh
    fload.sh cannot have a .sh ext.

--- INSTALL.sh
+++ INSTALL.sh
@@ -14,5 +14,5 @@ bash /home/$USER/Downloads/apt-abroad-main/setfunc_changemirror.sh
 
 chmod +x /home/$USER/.bash_functions.d/changemirror.sh
 source /home/$USER/.bash_functions.d/changemirror.sh
-chmod +x /home/$USER/.bash_functions.d/fload.sh
-source /home/$USER/.bash_functions.d/fload.sh
+chmod +x /home/$USER/.bash_functions.d/resync.load
+source /home/$USER/.bash_functions.d/resync.load
