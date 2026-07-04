32fc60b56b71d529db6ddf2805be841956c21794
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 19:42:23 +0200

    Update UNINSTALL.sh
    trying to avoid trashing wrong files

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -4,4 +4,4 @@ rm -R Downloads/apt-abroad-main*
 rm -R /etc/apt/apt-abroad
 rm -R /home/$USER/.bash_functions.d
 
-mv /etc/apt/sources.list.d/*.disabled /etc/apt/sources.list.d/debian.sources
+mv /etc/apt/sources.list.d/*MODEL.disabled /etc/apt/sources.list.d/debian.sources
