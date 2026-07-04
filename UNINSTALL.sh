15d35d408d2ad3ef908f23b2d594e88a2e23e162
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 22:40:39 +0200

    Update UNINSTALL.sh
    update to new file removals

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm -R Downloads/apt-abroad-main*
+rm -R /home/$USER/Downloads/apt-abroad-main
+rm /home/$USER/Downloads/apt-abroad-main.zip
+rm -R /home/$USER/Downloads/apt-abroad
 rm -R /etc/apt/apt-abroad
 rm -R /home/$USER/.bash_functions.d
+rm /etc/apt/sources.list.d/apt822IMMEDIATE.sources
 
 mv /etc/apt/sources.list.d/*MODEL.disabled /etc/apt/sources.list.d/debian.sources
