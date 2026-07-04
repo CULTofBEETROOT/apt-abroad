21843bbcd1513b56db3d769b12232c9e874e82a6
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 18:22:00 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,9 +4,15 @@
 wget -P /home/$USER/Downloads -O /home/$USER/Downloads/apt-abroad-main.zip https://github.com/CULTofBEETROOT/apt-abroad/archive/refs/heads/main.zip
 unzip /home/$USER/Downloads/apt-abroad-main.zip -d "Downloads"
 
+chmod +x /home/$USER/Downloads/apt-abroad-main/apt-httpsonly.sh
+chmod +x /home/$USER/Downloads/apt-abroad-main/set_funcenv.sh
+chmod +x /home/$USER/Downloads/apt-abroad-main/setfunc_changemirror.sh
+
 bash /home/$USER/Downloads/apt-abroad-main/apt-httpsonly.sh
 bash /home/$USER/Downloads/apt-abroad-main/set_funcenv.sh
 bash /home/$USER/Downloads/apt-abroad-main/setfunc_changemirror.sh
 
+chmod +x /home/$USER/.bash_functions.d/changemirror.sh
 source /home/$USER/.bash_functions.d/changemirror.sh
+chmod +x /home/$USER/.bash_functions.d/fload.sh
 source /home/$USER/.bash_functions.d/fload.sh
