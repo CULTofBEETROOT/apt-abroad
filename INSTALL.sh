600c647a075733d1c6063acc1c9a0f282e1bd37b
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 12:21:44 +0200

    Update INSTALL.sh
    in progress ..

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #install apt-abroad
 
-mv ~/apt-httpsonly.sh bash /home/$USER/Downloads/apt-httpsonly.sh
-mv ~/set_funcenv.sh /home/$USER/Downloads/set_funcenv.sh
-mv ~/setfunc_changemirror.sh /home/$USER/Downloads/setfunc_changemirror.sh
+unzip Downloads/apt-abroad-main.zip -d "Downloads"
 
-bash /home/$USER/Downloads/apt-httpsonly.sh
-bash /home/$USER/Downloads/set_funcenv.sh
-bash /home/$USER/Downloads/setfunc_changemirror.sh
+bash /home/$USER/Downloads/apt-abroad-main/apt-httpsonly.sh
+bash /home/$USER/Downloads/apt-abroad-main/set_funcenv.sh
+bash /home/$USER/Downloads/apt-abroad-main/setfunc_changemirror.sh
 
 source /home/$USER/.bash_functions.d/changemirror.sh
 source /home/$USER/.bash_functions.d/fload.sh
