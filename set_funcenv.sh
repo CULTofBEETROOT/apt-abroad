368be92d50238583489a1221eae6da10de6a7738
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 11:15:57 +0200

    Update set_funcenv.sh
    changed to $USER no longer /root/ or user

--- set_funcenv.sh
+++ set_funcenv.sh
@@ -5,13 +5,13 @@
 #source ~/.bash_functions.d/load.sh
 
 
-mkdir -p ~/.bash_functions.d
-touch ~/.bash_functions.d/load.sh
+mkdir -p /home/$USER/.bash_functions.d
+touch /home/$USER/.bash_functions.d/load.sh
 
-cat>~/.bash_functions.d/fload.sh<<'endOFfloadsh'
+cat>/home/$USER/.bash_functions.d/fload.sh<<'endOFfloadsh'
 #!/bin/bash
-#load all functions within ~/.bash_functions.d
-for f in ~/.bash_functions.d/*.sh; do
+#load all functions within /home/$USER/.bash_functions.d
+for f in /home/$USER/.bash_functions.d/*.sh; do
   [ -r "$f" ] && . "$f"
 done
 endOFfloadsh
