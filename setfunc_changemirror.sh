5f082e860e4d8b46a738a2f5aafc0ce617fba721
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 22:25:03 +0200

    Update setfunc_changemirror.sh
    redundant code

--- setfunc_changemirror.sh
+++ setfunc_changemirror.sh
@@ -14,9 +14,6 @@ mullvad relay list | grep -o '^............' | grep wg | grep -o '^....' | sort 
 cat /etc/apt/apt-abroad/urls.https | sed 's/https:\/\///g' | grep -o '^.*\/debian' | sed 's/.*\(..........\)$/\1/' | sort -u | grep "\." | sed 's/\/debian//g' | sed 's/\.//g' > /etc/apt/apt-abroad/ccDebianhttps.list
 grep -F -x -f /etc/apt/apt-abroad/ccMullvad.list /etc/apt/apt-abroad/ccDebianhttps.list | sed 's/^/&/' | sed 's/$/\/debian/' > /etc/apt/apt-abroad/ccDebianhttsMullvad.list
 awk 'NR==FNR{p[$0]=1; n++; next}{for (k in p) if (index($0,k)) {print; break}}' /etc/apt/apt-abroad/ccDebianhttsMullvad.list /etc/apt/apt-abroad/urls.https > /etc/apt/apt-abroad/debhttpsmulmirr.list
-echo $(cat /etc/apt/apt-abroad/debhttpsmulmirr.list | grep ".$movecc/debian" | tail -n 1) > /etc/apt/apt-abroad/debianIMMEDIATE.sources
-}
-cat /etc/apt/apt-abroad/debhttpsmulmirr.list | grep ".$movecc/debian" | tail -n 1 > /etc/apt/apt-abroad/debianIMMEDIATE.sources
 tur="$(cat /etc/apt/apt-abroad/debhttpsmulmirr.list | grep ".$movecc/debian" | tail -n 1)"
 sed "s|URIs: http:\/\/deb.debian.org\/debian\/|${tur}|g" /etc/apt/sources.list.d/apt822_MODEL.disabled > /etc/apt/sources.list.d/apt822IMMEDIATE.sources
 mullvad relay set location $movecc
