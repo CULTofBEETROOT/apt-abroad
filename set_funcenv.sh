227fff30995054dd7603c89c0213becfc29049c6
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-07-04 11:09:41 +0200

    Update set_funcenv.sh
    corrected cat typo

--- set_funcenv.sh
+++ set_funcenv.sh
@@ -8,7 +8,7 @@
 mkdir -p ~/.bash_functions.d
 touch ~/.bash_functions.d/load.sh
 
-cat>~/.bash_functions.d/fload.sh<'endOFfloadsh'
+cat>~/.bash_functions.d/fload.sh<<'endOFfloadsh'
 #!/bin/bash
 #load all functions within ~/.bash_functions.d
 for f in ~/.bash_functions.d/*.sh; do
