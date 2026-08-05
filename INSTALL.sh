85745292633b18a1e4d435f137565281a22cf4d9
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-08-06 00:39:11 +0200

    Update INSTALL.sh
    Signed-off-by: cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,10 +7,13 @@ unzip /home/$USER/Downloads/apt-abroad-main.zip -d "Downloads"
 chmod +x /home/$USER/Downloads/apt-abroad-main/apt-httpsonly.sh
 chmod +x /home/$USER/Downloads/apt-abroad-main/set_funcenv.sh
 chmod +x /home/$USER/Downloads/apt-abroad-main/setfunc_changemirror.sh
+chnod +x /home/$USER/Downloads/apt-abroad-main/install_Mullvad.sh
 
+bash /home/$USER/Downloads/apt-abroad-main/install_Mullvad.sh
 bash /home/$USER/Downloads/apt-abroad-main/apt-httpsonly.sh
 bash /home/$USER/Downloads/apt-abroad-main/set_funcenv.sh
 bash /home/$USER/Downloads/apt-abroad-main/setfunc_changemirror.sh
+
 
 chmod +x /home/$USER/.bash_functions.d/changemirror.sh
 source /home/$USER/.bash_functions.d/changemirror.sh
