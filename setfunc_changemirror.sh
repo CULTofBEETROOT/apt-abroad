a6f29bfdef9e4d982507438d72824b910752da89
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-09-04 19:59:35 +0200

    Update setfunc_changemirror.sh
    https://en.wikipedia.org/wiki/Mullvad#Donation_to_the_%C3%96rebro_Party
    
    this repo will not default to using mullvad any longer.
    
    Signed-off-by: cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>

--- setfunc_changemirror.sh
+++ setfunc_changemirror.sh
@@ -16,7 +16,7 @@ cat>/home/$USER/.bash_functions.d/changemirror.sh<<'endOFchangemirrorsh'
 changemirror () {
 movecc="$1";
 rm /etc/apt/sources.list.d/*IMMEDIATE.sources;
-mullvad relay list | grep -o '^............' | grep wg | grep -o '^....' | sort -u | sed 's/\t//g' > /etc/apt/apt-abroad/ccMullvad.list;
+#mullvad relay list | grep -o '^............' | grep wg | grep -o '^....' | sort -u | sed 's/\t//g' > /etc/apt/apt-abroad/ccMullvad.list;
 cat /etc/apt/apt-abroad/urls.https | sed 's/https:\/\///g' | grep -o '^.*\/debian' | sed 's/.*\(..........\)$/\1/' | sort -u | grep "\." | sed 's/\/debian//g' | sed 's/\.//g' > /etc/apt/apt-abroad/ccDebianhttps.list;
 grep -F -x -f /etc/apt/apt-abroad/ccMullvad.list /etc/apt/apt-abroad/ccDebianhttps.list | sed 's/^/&/' | sed 's/$/\/debian/' > /etc/apt/apt-abroad/ccDebianhttsMullvad.list;
 awk 'NR==FNR{p[$0]=1; n++; next}{for (k in p) if (index($0,k)) {print; break}}' /etc/apt/apt-abroad/ccDebianhttsMullvad.list /etc/apt/apt-abroad/urls.https > /etc/apt/apt-abroad/debhttpsmulmirr.list;
@@ -24,7 +24,7 @@ tscheuss="$(cat /etc/apt/apt-abroad/debhttpsmulmirr.list | grep ".$movecc/debian
 sed "s|URIs: http:\/\/deb.debian.org\/debian\/|URIs: ${tscheuss}|g" /etc/apt/sources.list.d/apt822_MODEL.disabled > /etc/apt/sources.list.d/apt822IMMEDIATE.sources;
 sed -i 's/https/tor+https/g' /etc/apt/sources.list.d/apt822IMMEDIATE.sources
 sed -i 's/http:/tor+https:/g' /etc/apt/sources.list.d/apt822IMMEDIATE.sources
-mullvad relay set location "$movecc";
+#mullvad relay set location "$movecc";
 sleep 10s;
 apt update && apt upgrade -y;
 }
