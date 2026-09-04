bd450edcb1e5815e324a4e91625082fb46875ea8
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-09-04 20:38:41 +0200

    Update setfunc_changemirror.sh
    https://en.wikipedia.org/wiki/Mullvad#Donation_to_the_%C3%96rebro_Party
    
    Tor solution in replacement.
    This repo will by default no longer be relying on mullvad.
    
    Signed-off-by: cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>

--- setfunc_changemirror.sh
+++ setfunc_changemirror.sh
@@ -2,7 +2,8 @@
 #compute the list of available mullvad / debian mirror matches that are https secure
 #change apt source (in 822 format) to a chosen exit location mirror 
 #the function changemirror is called with country (cc) code for input :
-# eg. : changemirror fi
+# eg. : changemirror fi for mullvad
+# eg. : changemirror fi for Tor
 if ls /etc/apt/*.list >/dev/null 2>&1; then
 apt-modernize sources
 fi
@@ -17,14 +18,18 @@ changemirror () {
 movecc="$1";
 rm /etc/apt/sources.list.d/*IMMEDIATE.sources;
 #mullvad relay list | grep -o '^............' | grep wg | grep -o '^....' | sort -u | sed 's/\t//g' > /etc/apt/apt-abroad/ccMullvad.list;
+sed 's/^.*,//g' /home/kxvesresc/Downloads/torexitIPs.txt | sed 's/UNKNOWN//g' | sort -u  | tr '[:upper:]' '[:lower:]' > /etc/apt/apt-abroad/ccTor.list
 cat /etc/apt/apt-abroad/urls.https | sed 's/https:\/\///g' | grep -o '^.*\/debian' | sed 's/.*\(..........\)$/\1/' | sort -u | grep "\." | sed 's/\/debian//g' | sed 's/\.//g' > /etc/apt/apt-abroad/ccDebianhttps.list;
-grep -F -x -f /etc/apt/apt-abroad/ccMullvad.list /etc/apt/apt-abroad/ccDebianhttps.list | sed 's/^/&/' | sed 's/$/\/debian/' > /etc/apt/apt-abroad/ccDebianhttsMullvad.list;
-awk 'NR==FNR{p[$0]=1; n++; next}{for (k in p) if (index($0,k)) {print; break}}' /etc/apt/apt-abroad/ccDebianhttsMullvad.list /etc/apt/apt-abroad/urls.https > /etc/apt/apt-abroad/debhttpsmulmirr.list;
-tscheuss="$(cat /etc/apt/apt-abroad/debhttpsmulmirr.list | grep ".$movecc/debian" | tail -n 1)";
+grep -F -x -f /etc/apt/apt-abroad/ccTor.list /etc/apt/apt-abroad/ccDebianhttps.list | sed 's/^/&/' | sed 's/$/\/debian/' > /etc/apt/apt-abroad/ccDebianhttsTor.list;
+awk 'NR==FNR{p[$0]=1; n++; next}{for (k in p) if (index($0,k)) {print; break}}' /etc/apt/apt-abroad/ccDebianhttsTor.list /etc/apt/apt-abroad/urls.https > /etc/apt/apt-abroad/debhttpsTormirr.list;
+tscheuss="$(cat /etc/apt/apt-abroad/debhttpsTormirr.list | grep ".$movecc/debian" | tail -n 1)";
 sed "s|URIs: http:\/\/deb.debian.org\/debian\/|URIs: ${tscheuss}|g" /etc/apt/sources.list.d/apt822_MODEL.disabled > /etc/apt/sources.list.d/apt822IMMEDIATE.sources;
 sed -i 's/https/tor+https/g' /etc/apt/sources.list.d/apt822IMMEDIATE.sources
 sed -i 's/http:/tor+https:/g' /etc/apt/sources.list.d/apt822IMMEDIATE.sources
 #mullvad relay set location "$movecc";
+chmod -x /home/$USER/bash_functions.d/torx.sh;
+source /home/$USER/bash_functions.d/torx.sh;
+torx "$movecc";
 sleep 10s;
 apt update && apt upgrade -y;
 }
