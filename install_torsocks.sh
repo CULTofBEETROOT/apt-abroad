07ba5117c2e4f0500538cb4558ee0e03f879b724
cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>
2026-09-04 23:28:14 +0200

    Update install_torsocks.sh
    tests on-going
    
    Signed-off-by: cultofbeetroot <186724066+CULTofBEETROOT@users.noreply.github.com>

--- install_torsocks.sh
+++ install_torsocks.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
-sudo apt update
 sudo apt install -y curl geoip-bin geoip-database torsocks torbrowser-launcher
+
+cat>/etc/apt/apt-abroad/torexits.sh<'endofsetfunc_torexitssh'
+torexits() {
 rm /etc/apt/apt-abroad/torexitIPs.txt
-{
-    printf 'ip,country_code\n'
+    {
+        printf 'ip,country_code\n'
 
-    curl -fsSL https://check.torproject.org/torbulkexitlist |
-    while IFS= read -r ip; do
-        [ -z "$ip" ] && continue
-        case "$ip" in \#*) continue ;; esac
+        curl -fsSL https://check.torproject.org/torbulkexitlist |
+        while IFS= read -r ip; do
+            [ -z "$ip" ] && continue
+            case "$ip" in \#*) continue ;; esac
 
-        country_code=$(
-            geoiplookup "$ip" 2>/dev/null |
-            sed -n 's/.*: \([A-Z][A-Z]\),.*/\1/p' |
-            head -n 1
-        )
-
-        printf '%s,%s\n' "$ip" "${country_code:-UNKNOWN}" >> /etc/apt/apt-abroad/torexitIPs.txt
-    done
-} | tee /etc/apt/apt-abroad/tor_exit_countries.csv
+            country_code=$(
+                geoiplookup "$ip" 2>/dev/null |
+                sed -n 's/.*: \([A-Z][A-Z]\),.*/\1/p' |
+                head -n 1
+            )
+    
+            printf '%s,%s\n' "$ip" "${country_code:-UNKNOWN}" >> /etc/apt/apt-abroad/torexitIPs.txt
+        done
+    } | tee /etc/apt/apt-abroad/tor_exit_countries.csv
+}
+endofsetfunc_torexitssh
